d7cc6384eb8b977b757cf6f4c96cf34e96c97cf9
Draicor <draicor@outlook.com>
2025-12-28 17:12:18 -0400

    2025-12-28 #2
    - Integrated the EOS Integration Kit plugin.
    - Added a new input action, IA_Cursor, set it to the left alt key, to toggle the mouse cursor and change the input mode from game to game and UI to click on UI menus.
    - Created a new product in the epic games dev website and configurated the client to connect to it 10 seconds after starting the game, to prompt the epic games login, and its working.

---
+++ Source/Spellstrike/Spellstrike.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Spellstrike.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Spellstrike, "Spellstrike" );
